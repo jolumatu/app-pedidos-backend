468fe36deede7acf06b03fdeb7abd815d2bc2e7f
Jose Luis Martinez Tunarroza <jl.martinez.tunarroza@outlook.com>
2022-10-19 19:32:43 -0500

    version 1.0.0 de la app de pedidos

---
+++ src/models/person.model.ts
@@ -0,0 +1,55 @@
+import {Entity, model, property, hasMany} from '@loopback/repository';
+import {Order} from './order.model';
+
+@model()
+export class Person extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+    generated: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  name: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  lastName: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  phoneNumber: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @hasMany(() => Order)
+  orders: Order[];
+
+  constructor(data?: Partial<Person>) {
+    super(data);
+  }
+}
+
+export interface PersonRelations {
+  // describe navigational properties here
+}
+
+export type PersonWithRelations = Person & PersonRelations;
